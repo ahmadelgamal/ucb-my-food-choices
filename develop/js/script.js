30b5d2991a1b4862966c7b3650c1bc3aa61a07ab
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-10-30 16:24:58 -0700

    Adjusts table column-widths in reports.html

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,7 +1,13 @@
-// For side navbar. As per materialize docs
+// for side navbar, as per materialize docs
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  const elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems, {});
+});
+
+// for dropdown buttons, as per materialize docs
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {});
 });
 
 M.AutoInit();
