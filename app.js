671375b663a0ef90e84d35da8177b0d52ef33a4f
Brian Lopez <you@example.com>
2020-10-30 23:03:30 -0700

    initial site navigation - pre seperation of concerns

--- app.js
+++ app.js
@@ -3,34 +3,57 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
+// log requests to console
+app.use((req, res, next) => {
+    console.log('new request:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+// static files
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
+});
+
+app.get('/index.html', (req, res) => {
+    res.render('index');
 });
 
 app.get('/about.html', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
 app.get('/contact.html', (req, res) => {
-    res.sendFile('./views/contact.html', { root: __dirname });
+    res.render('contact');
 });
 
 app.get('/events.html', (req, res) => {
-    res.sendFile('/views/events.html', { root: __dirname });
+    res.render('events');
+});
+
+app.get('/login.html', (req, res) => {
+    res.render('index');
 });
 
 app.get('/restrictions.html', (req, res) => {
-    res.sendFile('/views/restrictions.html', { root: __dirname });
+    res.render('restrictions');
 });
 
 app.get('/signup.html', (req, res) => {
-    res.sendFile('/views/signup.html', { root: __dirname });
+    res.render('signup');
 });
 
 // 404
 app.use((req, res) => {
-    res.sendFile('/views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
