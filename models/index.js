e1e7a3420717095b90640090554e51d51af98e7b
Ann-Marie <anniedillier@gmail.com>
2020-11-01 20:49:56 -0800

    routes

--- models/index.js
+++ models/index.js
@@ -1,43 +1,22 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// import all models
+const User = require("./User");
+const Event = require("./Event");
+const Restriction = require("./Restriction");
 
-// create our Event model
-class Event extends Model {}
+User.hasMany(Restriction, {
+  foreignKey: "user_id",
+});
 
-// create fields/columns for Event model
-Event.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    event_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    event_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        isDate: true,
-      },
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "event",
-  }
-);
+User.hasMany(Event, {
+  foreignKey: "user_id",
+});
 
-module.exports = Event;
+Event.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Restriction.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Event, Restriction };
