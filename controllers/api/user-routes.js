86ffcf250a87faa9904bc5a5116989e0826a582f
Ann-Marie <anniedillier@gmail.com>
2020-11-01 20:57:12 -0800

    auth

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -122,7 +122,7 @@ router.post("/login", (req, res) => {
       // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
+      req.session.loggedIn = true; 
 
       res.json({ user: dbUserData, message: "You are now logged in!" });
     });
