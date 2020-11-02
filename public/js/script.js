cbcc12d963afb69c03771d2aefc8407098bc078c
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-01 21:12:02 -0800

    restores my additions to script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,11 @@
-// For side navbar. As per materialize docs
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  // init side navbar, as per materialize docs
+  const sideNavEls = document.querySelectorAll('.sidenav');
+  const sideNavInstances = M.Sidenav.init(sideNavEls, {});
+
+  // init dropdown buttons, as per materialize docs
+  const dropDownEls = document.querySelectorAll('.dropdown-trigger');
+  const dropDownInstances = M.Dropdown.init(dropDownEls, {});
 });
 
 M.AutoInit();
