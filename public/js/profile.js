ad025bdaacf499f574b039b376321fa07ec15fc0
Ann-Marie <anniedillier@gmail.com>
2020-11-07 17:18:49 -0800

    fix post profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,32 +1,30 @@
-const restrict = document.forms[0];
-
 async function profileFormHandler(event) {
   event.preventDefault();
 
   const restrictionsArray = [];
-  //const restrict = document.forms[0];
+  const restrict = document.forms[0];
 
   for (var i = 0; i < restrict.length; i++) {
     if (restrict[i].checked) {
       restrictionsArray.push(restrict[i].value);
-      console.log(restrictionsArray);
     }
-
-    // for each element (checkbox)...
-   restrictionsArray.forEach(checkPostData);
-
-    // check if item has a value & post data
-    async function checkPostData(item) {
-      const response = await fetch("/api/profiles", {
-        method: "post",
-        body: JSON.stringify({ restriction_id: item }),
-        headers: { "Content-Type": "application/json" },
-      });
-      if (response.ok) {
-        console.log(item);
-      } else {
-        alert(response.statusText);
-      }
+  }
+  console.log(restrictionsArray);
+  // for each element (checkbox)...
+  restrictionsArray.forEach(checkPostData);
+  console.log("second");
+  console.log(restrictionsArray);
+  // check if item has a value & post data
+  async function checkPostData(item) {
+    const response = await fetch("/api/profiles", {
+      method: "post",
+      body: JSON.stringify({ restriction_id: item }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      console.log(item);
+    } else {
+      alert(response.statusText);
     }
   }
 }
@@ -47,6 +45,6 @@ async function profileFormHandler(event) {
 document
   .querySelector(".profile-form")
   .addEventListener("submit", profileFormHandler);
-  // document
-  // .querySelector(".profile-form")
-  // .addEventListener("submit", deleteProfileFormHandler);
+// document
+// .querySelector(".profile-form")
+// .addEventListener("submit", deleteProfileFormHandler);
