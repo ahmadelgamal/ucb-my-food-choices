d474660d5d5be021322234793ef6cccd5c1b1016
Ann-Marie <anniedillier@gmail.com>
2020-11-07 16:32:32 -0800

    develop pull

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,34 +6,34 @@ async function profileFormHandler(event) {
   const restrict = document.forms[0];
 
   for (var i = 0; i < restrict.length; i++) {
-   // if (restrict[i].checked) {
-      restrictionsArray.push(restrict[i].value)
+    if (restrict[i].checked) {
+      restrictionsArray.push(restrict[i].value);
       console.log(restrictionsArray);
 
-    //  if (restrict[i].unchecked) {
-    //     notRestrictedArray.push(restrict[i].value)
-    //     console.log(notRestrictedArray);
-
-      }
+      // if (restrict[i].unchecked) {
+      //   notRestrictedArray.push(restrict[i].value);
+      //   console.log(notRestrictedArray);
+      // }
     }
 
-// for each element (checkbox)...
-restrictionsArray.forEach(checkPostData);
+    // for each element (checkbox)...
+    restrictionsArray.forEach(checkPostData);
 
-// check if item has a value & post data
-async function checkPostData(item) {
-  const response = await fetch("/api/profiles", {
-    method: "post",
-    body: JSON.stringify({ restriction_id: item }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-   console.log(item);
-  } else {
-    alert(response.statusText);
+    // check if item has a value & post data
+    async function checkPostData(item) {
+      const response = await fetch("/api/profiles", {
+        method: "post",
+        body: JSON.stringify({ restriction_id: item }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        console.log(item);
+      } else {
+        alert(response.statusText);
+      }
+    }
   }
 }
-
 
 // event listener
 document
