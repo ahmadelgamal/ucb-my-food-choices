c32032a41e11bb7d06ee20fc271a086089efde76
Ann-Marie <anniedillier@gmail.com>
2020-11-07 16:17:51 -0800

    loging

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,41 +1,52 @@
+const restrict = document.forms[0];
+
 async function profileFormHandler(event) {
   event.preventDefault();
 
   const restrictionsArray = [];
- // const notRestrictedArray = [];
-  const restrict = document.forms[0];
+  //const restrict = document.forms[0];
 
   for (var i = 0; i < restrict.length; i++) {
-   // if (restrict[i].checked) {
-      restrictionsArray.push(restrict[i].value)
+    if (restrict[i].checked) {
+      restrictionsArray.push(restrict[i].value);
       console.log(restrictionsArray);
-
-    //  if (restrict[i].unchecked) {
-    //     notRestrictedArray.push(restrict[i].value)
-    //     console.log(notRestrictedArray);
-
-      }
     }
 
-// for each element (checkbox)...
-restrictionsArray.forEach(checkPostData);
+    // for each element (checkbox)...
+   restrictionsArray.forEach(checkPostData);
 
-// check if item has a value & post data
-async function checkPostData(item) {
-  const response = await fetch("/api/profiles", {
-    method: "post",
-    body: JSON.stringify({ restriction_id: item }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-   console.log(item);
-  } else {
-    alert(response.statusText);
+    // check if item has a value & post data
+    async function checkPostData(item) {
+      const response = await fetch("/api/profiles", {
+        method: "post",
+        body: JSON.stringify({ restriction_id: item }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        console.log(item);
+      } else {
+        alert(response.statusText);
+      }
+    }
   }
 }
 
+// async function deleteProfileFormHandler(event) {
+//   event.preventDefault();
+
+//   const notRestrictedArray = [];
+//   for (var i = 0; i < restrict.length; i++) {
+//     if (restrict[i].unchecked) {
+//       notRestrictedArray.push(restrict[i].value);
+//       console.log(notRestrictedArray);
+//     }
+//   }
+// }
 
 // event listener
 document
   .querySelector(".profile-form")
   .addEventListener("submit", profileFormHandler);
+  // document
+  // .querySelector(".profile-form")
+  // .addEventListener("submit", deleteProfileFormHandler);
