6a1faa95086243dc58d32d98da1c3414cb31b15b
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-08 23:26:07 -0800

    Completes project

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,47 +10,31 @@ async function profileFormHandler(event) {
       restrictionsArray.push(restrict[i].value);
     }
   }
+
   // for each element (checkbox)...
   restrictionsArray.forEach(checkPostData);
+
   // check if item has a value & post data
   async function checkPostData(item) {
     const response = await fetch(`api/profiles/restriction/` + item,
       {
         method: "get",
-
-        //headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json" }
       }
     );
     if (response.ok) {
-
-      // } else {
-      //   const response = await fetch("/api/profiles", {
-      //     method: "post",
-      //     body: JSON.stringify({ restriction_id: item }),
-      //     headers: { "Content-Type": "application/json" },
-      //   });
-
-
+      console.log(response);
+    } else {
+      const response = await fetch("/api/profiles", {
+        method: "post",
+        body: JSON.stringify({ restriction_id: item }),
+        headers: { "Content-Type": "application/json" },
+      });
     }
   }
 }
-
-// async function deleteProfileFormHandler(event) {
-//   event.preventDefault();
-
-//   const notRestrictedArray = [];
-//   for (var i = 0; i < restrict.length; i++) {
-//     if (restrict[i].unchecked) {
-//       notRestrictedArray.push(restrict[i].value);
-//       console.log(notRestrictedArray);
-//     }
-//   }
-// }
 
 // event listener
 document
   .querySelector(".profile-form")
   .addEventListener("submit", profileFormHandler);
-// document
-// .querySelector(".profile-form")
-// .addEventListener("submit", deleteProfileFormHandler);
