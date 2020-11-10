72f970358f31d35d72a8600b64760b24840f7158
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-09 20:58:32 -0800

    removes setTimeout that is not used

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -199,7 +199,6 @@ router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy();
     res.clearCookie('connect.sid', { path: '/' }).status(200).send('OK');
-    res.session.loggedIn = false;
     return;
   }
   else {
