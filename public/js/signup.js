e759a722df97254fd72fbc1f673bca2a4a891093
Aaditi <aadi2007.aaditi@gmail.com>
2020-11-11 11:51:44 -0800

    add admin signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,30 +5,55 @@ async function signupHandler(event) {
   const last_name = document.querySelector("#last_name").value.trim();
   const email = document.querySelector("#email").value.trim();
   const password = document.querySelector("#password").value.trim();
+  const selectSignUpEl = document.querySelector('.browser-default');
 
   if (first_name && last_name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        email,
-        password,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    // check the response status
-    if (response.ok) {
-      M.toast({ html: 'You have successfully signed up' });
-      document.location.replace("/");
-    } else {
-      M.toast({ html: 'User already exists.' });
-    }
-  } else {
-    M.toast({ html: 'Please fill all fields.' });
+    if (selectSignUpEl.value === '1'){
+      const response = await fetch("/api/users", {
+        method: "post",
+        body: JSON.stringify({
+          first_name,
+          last_name,
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      // check the response status
+      if (response.ok) {
+        M.toast({ html: 'You have successfully signed up' });
+        document.location.replace("/");
+      } else {
+        M.toast({ html: 'User already exists.' });
+      }
+    } 
+    else{
+      const response = await fetch("/api/admin", {
+        method: "post",
+        body: JSON.stringify({
+          first_name,
+          last_name,
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      // check the response status
+      if (response.ok) {
+        M.toast({ html: 'You have successfully signed up' });
+        document.location.replace("/");
+      } else {
+        M.toast({ html: 'User already exists.' });
+      }
+    } 
+}
+  else {
+     M.toast({ html: 'Please fill all fields.' });
   }
 }
+
 
 document
   .querySelector(".signup-form")
