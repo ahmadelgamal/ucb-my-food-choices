e09375344a4eb02d4eb6befc15a1f064460e1050
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-11 21:52:14 -0800

    updates jQuery for materialize

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function () {
   // init sidenav
   $('.sidenav').sidenav();
-  // init dropdown
-  $('.dropdown-button').dropdown({
+
+  // init navbar dropdown
+  $('.dropdown-trigger').dropdown({
     constrainWidth: false, // Does not change width of dropdown to that of the activator
     hover: true, // Activate on hover
-    coverTrigger: false, // Displays dropdown below the button
-  }
-  );
+    coverTrigger: false // Displays dropdown below the button
+  });
+  // init dropdown button
+  // $('.dropdown-trigger').dropdown();
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems, {
+//     constrainWidth: false, // Does not change width of dropdown to that of the activator
+//     hover: true, // Activate on hover
+//     coverTrigger: false // Displays dropdown below the button
+//   });
+// });
+
 
 // init materialize components
 M.AutoInit();
