1feb810467dd34fc7792bcd9ee672d7e2eb24fdb
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-11 20:33:52 -0800

    Adds jQuery code for dropdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
   // init sidenav
   $('.sidenav').sidenav();
-  // init navbar dropdown
-  $(".dropdown-trigger").dropdown();
-  // $('.dropdown-trigger').attr("data-constrainWidth", false);
-  // $('.dropdown-trigger').attr("data-coverTrigger", false);
+  // init dropdown
+  $('.dropdown-button').dropdown({
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    coverTrigger: false, // Displays dropdown below the button
+  }
+  );
 });
 
 // init materialize components
