d14050076adaa081b3558f5dc2c781f2472aedb7
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-12 22:47:05 -0800

    edits signup logic

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,28 +5,56 @@ async function signupHandler(event) {
   const last_name = document.querySelector("#last_name").value.trim();
   const email = document.querySelector("#email").value.trim();
   const password = document.querySelector("#password").value.trim();
+  const confirmPassword = document.querySelector('#confirm-password').value.trim();
+  const selectSignUpEl = document.querySelector('.browser-default');
 
-  if (first_name && last_name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        email,
-        password,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    // check the response status
-    if (response.ok) {
-      M.toast({ html: 'You have successfully signed up' });
-      document.location.replace("/");
-    } else {
-      M.toast({ html: 'User already exists.' });
-    }
+  if (password !== confirmPassword) {
+    M.toast({ html: 'Password does not match' });
   } else {
-    M.toast({ html: 'Please fill all fields.' });
+    if (first_name && last_name && email && password) {
+      if (selectSignUpEl.value === '1') {
+        const response = await fetch("/api/users", {
+          method: "post",
+          body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            password,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        // check the response status
+        if (response.ok) {
+          M.toast({ html: 'You have successfully signed up' });
+          document.location.replace("/");
+        } else {
+          M.toast({ html: 'User already exists.' });
+        }
+      }
+      else {
+        const response = await fetch("/api/admin", {
+          method: "post",
+          body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            password,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        // check the response status
+        if (response.ok) {
+          M.toast({ html: 'You have successfully signed up' });
+          document.location.replace("/");
+        } else {
+          M.toast({ html: 'User already exists.' });
+        }
+      }
+    } else {
+      M.toast({ html: 'Please fill all fields.' });
+    }
   }
 }
 
