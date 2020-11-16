0d17ea7145cacd1d1ffa884d2ade0bcc6c630b06
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-15 19:24:22 -0800

    Refactors changePw.js

--- public/js/changePw.js
+++ public/js/changePw.js
@@ -1,55 +1,41 @@
 const loggedIn = document.querySelector("#current-password").dataset.loggedin;
+
 async function changePwFormHandler(event) {
   event.preventDefault();
 
-  const currpw = document.querySelector("#current-password").value.trim();
-  const newpw = document.querySelector("#new-password").value.trim();
-
-  //const userId = document.createElement();
+  const currentPassword = document.querySelector("#current-password").value.trim();
+  const newPassword = document.querySelector("#new-password").value.trim();
+  const confirmNewPassword = document.querySelector("#confirm-new-password").value.trim();
+  const userId = document.querySelector("#current-password").name;
 
   //select guest or host login 
 
-  const id = document.querySelector("#current-password").name;
-
-  console.log(loggedIn);
-  if (loggedIn === "guestLoggedIn") {
-    if (currpw.length < 8) {
-      M.toast({ html: 'Password must be at least 8 characters long' });
-      if (currpw && newpw) {
-        const response = await fetch(`/api/users/${id}`, {
-          method: "put",
-          body: JSON.stringify({ password: newpw }),
-          headers: { "Content-Type": "application/json" },
-        });
-
-        if (response.ok) {
-          M.toast({ html: 'Password changed successfully' });
-          document.location.replace('/');
-        } else {
-          M.toast({ html: 'Error!' });
-        }
-      }
-    }
+  if (newPassword.length < 8) {
+    M.toast({ html: 'Password must be at least 8 characters long' });
+  } else if (newPassword !== confirmNewPassword) {
+    M.toast({ html: 'Password does not match' });
   }
-
-  if (loggedIn === "hostLoggedIn") {
-    console.log("Entered here");
+  if (loggedIn === "guestLoggedIn") {
+    const response = await fetch(`/api/users/${id}`, {
+      method: "put",
+      body: JSON.stringify({ password: newPassword }),
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (loggedIn === "hostLoggedIn") {
     if (currpw && newpw) {
       const response = await fetch(`/api/admin/${id}`, {
         method: "put",
         body: JSON.stringify({ password: newpw }),
         headers: { "Content-Type": "application/json" },
       });
-
-      if (response.ok) {
-        M.toast({ html: 'Password changed successfully' });
-        document.location.replace('/');
-      } else {
-        M.toast({ html: 'Error!' });
-      }
     }
   }
-
+  if (response.ok) {
+    M.toast({ html: 'Password changed successfully' });
+    document.location.replace('/');
+  } else {
+    M.toast({ html: 'Error!' });
+  }
 }
 
 document
